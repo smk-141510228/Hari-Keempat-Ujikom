52dbbecff05e43b014ab2fe25d5e8421a1a36342
smk-141510228 <smk-141510228>
2017-02-24 11:20:08 +0700

    Hari Keempat Ujikom

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+        <div class="col-10-0">
+            <div class=" panel-default">
+           
+             <center><font size="5" face="comic sans ms">SELAMAT DATANG DI APLIKASI PENGGAJIAN</font><br></center></div>
+             <br>
+             <br>
+             <br>
+             <center><img src="images.jpg"></center>
+             <br>
+             <br>
+             <br>
+             <center><font size="5" face="comic sans ms">SMK ASSALAAM BANDUNG</font><br></center></div>
+   
+    </div>
+</div>
+@endsection
